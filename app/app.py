b8f6a3e9be8696d0c430f94c3bdefc7f12cde902
Jan-Philippe Lavoie <jan-philippe.lavoie@ixiasoft.com>
2017-11-27 14:39:44 -0500

    Fixed database import issues

--- app/app.py
+++ app/app.py
@@ -20,11 +20,12 @@ def root():
 
 @app.route('/players')
 def list_players():
-    text = ""
-    players = models.Player.query.all()
-    for player in players:
-        text += str(player) + '\n'
-    return text
+    return list_table(models.Player)
+
+
+@app.route('/games')
+def list_games():
+    return list_table(models.Game)
 
 
 @app.route('/config')
@@ -41,10 +42,10 @@ def config():
 def go():
     if request.method == 'POST':
         champion_name = request.form['champion']
-        player_id = models.Player.query.filter_by(username=champion_name).first().id
+        champion = models.Player.query.filter_by(username=champion_name).first()
         go = models.Game.query.filter_by(name='go').first()
-        go.champion_id = player_id
-        db.session.commit()
+        go.champion_id = champion.id
+        models.db.session.commit()
         app.logger.debug("New champion: {}".format(champion_name))
     else:
         go = models.Game.query.filter_by(name='go').first()
@@ -54,6 +55,14 @@ def go():
             champion_name = 'Personne'
 
     return render_template('go.html', name=champion_name)
+
+
+def list_table(model):
+    html = ""
+    entries = model.query.all()
+    for entry in entries:
+        html += "<div>{}</div>".format(entry)
+    return html
 
 
 if __name__ == "__main__":
